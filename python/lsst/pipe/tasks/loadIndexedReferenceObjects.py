d888a59158f01cc8d1a2b274bb4e7d1991b10917
Simon Krughoff <simon.krughoff@gmail.com>
2016-03-23 22:17:15 -0700

    Add docs for the ref loader

--- python/lsst/pipe/tasks/loadIndexedReferenceObjects.py
+++ python/lsst/pipe/tasks/loadIndexedReferenceObjects.py
@@ -52,6 +52,20 @@ class LoadIndexedReferenceObjectsTask(LoadReferenceObjectsTask):
 
     @pipeBase.timeMethod
     def loadSkyCircle(self, ctrCoord, radius, filterName=None):
+        """!Load reference objects that overlap a circular sky region
+
+        @param[in] ctrCoord  center of search region (an lsst.afw.geom.Coord)
+        @param[in] radius  radius of search region (an lsst.afw.geom.Angle)
+        @param[in] filterName  name of filter, or None for the default filter;
+            used for flux values in case we have flux limits (which are not yet implemented)
+
+        @return an lsst.pipe.base.Struct containing:
+        - refCat a catalog of reference objects with the
+            \link meas_algorithms_loadReferenceObjects_Schema standard schema \endlink
+            as documented in LoadReferenceObjects, including photometric, resolved and variable;
+            hasCentroid is False for all objects.  None if no ref objects available.
+        - fluxField = name of flux field for specified filterName.  None if refCat is None.
+        """
         id_list, boundary_mask = self.indexer.get_pixel_ids(ctrCoord, radius)
         shards = self.get_shards(id_list)
         refCat = self.butler.get(self.ref_dataset_name, dataId=self.make_data_id('master_schema'),
@@ -71,6 +85,11 @@ class LoadIndexedReferenceObjectsTask(LoadReferenceObjectsTask):
         )
 
     def get_shards(self, id_list):
+        """!Get all shards that touch a circular aperture
+
+        @param[in] id_list  A list of integer pixel ids
+        @param[out] a list of SourceCatalogs for each pixel, None if not data exists
+        """
         shards = []
         for pixel_id in id_list:
             if self.butler.datasetExists(self.ref_dataset_name, dataId=self.make_data_id(pixel_id)):
@@ -79,6 +98,13 @@ class LoadIndexedReferenceObjectsTask(LoadReferenceObjectsTask):
         return shards
 
     def _trim_to_circle(self, catalog_shard, ctrCoord, radius):
+        """!Trim a catalog to a circular aperture.
+
+        @param[in] catalog_shard  SourceCatalog to be trimmed
+        @param[in] ctrCoord  afw.Coord to compare each record to
+        @param[in] radius  afwGeom.Angle indicating maximume separation
+        @param[out] a SourceCatalog constructed from records that fall in the circular aperture
+        """
         temp_cat = type(catalog_shard)(catalog_shard.schema)
         for record in catalog_shard:
             if record.getCoord().angularSeparation(ctrCoord) < radius:
